b99f82602f1fbadb938fb438e066d705cf92e745
Khalid Aqeeli <aqeeli@protonmail.com>
2021-06-09 18:21:23 +0300

    add ItemViewModelDelegate file

---
+++ TodoList/Protocols/ItemViewModelDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ItemViewModelDelegate.swift
+//  TodoList
+//
+//  Created by Khalid Aqeeli on 09/06/2021.
+//
+
+import Foundation
+
+protocol ItemViewModelDelegate {
+    func didFinishUpdates(finished: Bool)
+}
